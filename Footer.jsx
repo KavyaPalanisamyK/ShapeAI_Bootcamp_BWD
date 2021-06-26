d498da55b1337b82123aada61e505d2fc12208a4
KavyaPalanisamyK <86522422+KavyaPalanisamyK@users.noreply.github.com>
2021-06-26 18:50:32 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+  return(
+    <footer>
+      <p>Copyright by Kavya @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
